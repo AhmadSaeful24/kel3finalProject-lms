7f8e85ab1b6e9359b592233144091bc611364692
AhmadSaeful24 <ahmadsaefulcahya30@gmail.com>
2023-08-16 10:33:28 +0700

    menambahkan nav banner, footer

--- frontend/src/pages/pusatPelatihan.js
+++ frontend/src/pages/pusatPelatihan.js
@@ -1,8 +1,15 @@
-import Nav from "../components/Layouts/nav/navigation.js";
+import Nav from "../components/navbar/Navigation";
+import Banners from "../controllers/banner/banner";
+import Pcard from "../controllers/card/Pcard";
+import Footer from "../components/footer/footer";
+
 const PusatPel = () => {
     return(
         <div className="PusPel">
             <Nav />
+            <Banners />
+            <Pcard />
+            <Footer />
         </div>
     )
 }
